4866e7e8b29bfed7e4d6959a3098926124157451
Musab Guma'a <musabgumaa@gmail.com>
2022-10-30 12:01:51 +0000

    Final commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,44 +3,19 @@ import './App.css';
 
 
 import Painting from './Painting.tsx';
-import { Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import React from 'react';
 import { ReactComponent as LogoSvg } from "./logo.svg";
-import { EventEmitter } from 'stream';
+import { PaintingT } from './Painting';
 
 let interval;
 let scrollInterval;
 
-function distribute(imgs, buckets, pop: boolean) {
-  let step: number = imgs.length / buckets.length;
-
-  for (let i = 0; i < buckets.length; i++) {
-    for (let j = i * step; j < (i + 1) * step; j++) {
-      buckets[i].push(movingImage(imgs[j], i % 2 === 0));
-      // if (pop) buckets[i].pop();
-    }
-  }
-
-  return buckets;
-}
-
-function movingImage(painting: any, isLeftMoving: boolean) {
-  return <Painting
-    key={Math.random()}
-    url={painting.url}
-    url_large={painting.url_large}
-    artist={painting.author}
-    isLeftMoving={isLeftMoving}
-    title={painting.title}
-    created={painting.created}
-    likes={painting.likes}
-  />;
-}
-
 type Props = {};
 type State = {
   imageRow: any[],
-  page: number
+  page: number,
+  selected?: PaintingT
 };
 
 
@@ -61,26 +36,52 @@ export default class App extends React.Component<Props, State> {
   }
 
   loadImages = async () => {
-    await fetch(`http://20.0.0.86:3000/list/30/${this.state.page}`)
+    await fetch(`http://20.0.0.86:3000/group/true/30/${this.state.page}`)
       .then(res => res.json())
       .then(data => {
         this.setState({
-          imageRow: distribute(
+          imageRow: this.distribute(
             data,
             this.state.imageRow,
             false //this.state.page > 2
           ),
           page: this.state.page + 1
-        }, () => {
-          console.log(data);
         });
       });
+  }
+
+  distribute = (imgs, buckets, pop: boolean) => {
+    let step: number = imgs.length / buckets.length;
+
+    for (let i = 0; i < buckets.length; i++) {
+      for (let j = i * step; j < (i + 1) * step; j++) {
+        buckets[i].push(this.movingImage(imgs[j], i % 2 === 0));
+        // if (pop) buckets[i].pop();
+      }
+    }
+
+    return buckets;
+  }
+
+  movingImage = (painting: PaintingT, isLeftMoving: boolean) => {
+    return <Painting
+      key={Math.random()}
+      painting={painting}
+      isLeftMoving={isLeftMoving}
+      showOverlay={this.selectPainting}
+    />;
+  }
+
+  selectPainting = (painting: PaintingT) => {
+    this.setState({
+      selected: painting
+    });
   }
 
   componentDidMount = () => {
     this.loadImages();
     this.loadImages();
-    interval = setInterval(this.loadImages, 10000);
+    interval = setInterval(this.loadImages, 3000);
   }
 
   componentWillUnmount = () => {
@@ -88,10 +89,123 @@ export default class App extends React.Component<Props, State> {
     clearInterval(scrollInterval);
   }
 
+  incrementLikesOnSelected = async () => {
+    if (!this.state.selected) return;
+
+    const likedPaintings = JSON.parse(localStorage.getItem("likedPaintings") || "[]");
+    if (likedPaintings.includes(this.state.selected.id)) return;
+
+    likedPaintings.push(this.state.selected.id);
+    const selected = this.state.selected;
+    selected.likes = selected.likes ? selected.likes + 1 : 1;
+    this.setState({
+      selected: selected
+    });
+    localStorage.setItem("likedPaintings", JSON.stringify(likedPaintings));
+    /*await fetch(`http://20.0.0.86:3000/like/${selected.id}`, {
+      method: "POST"
+    });*/
+  }
+
+  seeMoreLikeThis = async () => {
+    if (!this.state.selected) return;
+
+    clearInterval(interval);
+    await fetch(`http://20.0.0.86:3000/similar/${this.state.selected.id}/30/0`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          imageRow: this.distribute(
+            data,
+            [[], [], []],
+            false //this.state.page > 2
+          ),
+          page: 0,
+          selected: undefined
+        });
+      });
+    this.loadImages();
+    interval = setInterval(this.loadImages, 3000);
+  }
+
   render() {
     return (
       <>
         <Container style={{ overflow: "hidden" }} fluid>
+          {
+            this.state.selected && (
+              <Container fluid
+                style={{
+                  position: "absolute",
+                  width: "100vw",
+                  height: "100vh",
+                  zIndex: 9999,
+                  top: 0, left: 0,
+                  background: "rgba(0, 0, 0, .8)",
+                  color: "rgba(255, 255, 255, .6)",
+                  textAlign: "center",
+                  paddingTop: "5vh"
+                }}
+
+                onClick={() => {
+                  console.log("Overlay!");
+                  // this.props.showOverlay();
+                  // this.setState({ selected: false });
+                }}
+              >
+                <Row>
+                  <h1 style={{ fontFamily: "Crimson Text", marginTop: 20, padding: "25px 0px" }}>
+                    <span style={{ fontStyle: "italic", fontWeight: "bold", color: "rgba(255, 255, 255, .85)" }}>{this.state.selected.title}</span> &nbsp;by {this.state.selected.author}{this.state.selected.created && ", " + this.state.selected.created}
+                  </h1>
+                </Row>
+                <Row style={{ padding: 10 }}>
+                  <Col>
+                    <img
+                      src={this.state.selected.url_large}
+                      style={{
+                        maxHeight: "60vh",
+                        width: "auto",
+                        padding: 0
+                      }}
+                      alt="something cool"
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Button
+                      style={{
+                        background: "rgba(255, 255, 255, 1)",
+                        border: 0,
+                        color: "black",
+                        fontWeight: "bold",
+                        marginTop: 20,
+                        marginBottom: 10
+                      }}
+                      onClick={this.seeMoreLikeThis}
+                    >
+                      See More Like This
+                    </Button>
+                    <Button
+                      hidden
+                      style={{
+                        background: "rgba(220, 53, 69, 1)",
+                        border: 0,
+                        color: "white",
+                        fontWeight: "bold",
+                        marginTop: 20,
+                        marginBottom: 10
+                      }}
+                      onClick={this.incrementLikesOnSelected}
+                    >
+                      {this.state.selected.likes || 0} Like{!(this.state.selected.likes && this.state.selected.likes === 1) && "s"}
+                    </Button>
+                  </Col>
+                </Row>
+                <br />
+              </Container>
+            )
+          }
           <div
             style={{
               position: "fixed",
@@ -153,6 +267,7 @@ export default class App extends React.Component<Props, State> {
                   flexDirection: (x % 2 !== 0 ? "row-reverse" : "row"),
                   flexWrap: "nowrap"
                 }}
+                className={x % 2 === 0 ? "moveHorizontalLeft" : "moveHorizontalRight"}
               >
                 {this.state.imageRow[x]}
               </Row>
--- src/Painting.tsx
+++ src/Painting.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 
-type Props = {
-    isLeftMoving: boolean,
+export type PaintingT = {
+    id: string,
     url: string,
     url_large: string,
     artist: string,
     title: string,
     created?: string,
     likes?: number
+};
+
+type Props = {
+    isLeftMoving: boolean,
+    painting: PaintingT,
+    showOverlay: (p: PaintingT) => void
 };
 type State = {
     selected: boolean
@@ -35,86 +41,12 @@ export default class Painting extends React.Component<Props, State> {
         return (
             <>
                 <img
-                    src={this.props.url}
+                    src={this.props.painting.url}
                     style={{ height: "100%", width: "auto", padding: 0 }}
                     className={(this.props.isLeftMoving ? "moveHorizontalLeft" : "moveHorizontalRight") + " painting"}
                     alt="something cool"
-                    onClick={this.handleSelect}
+                    onClick={() => this.props.showOverlay(this.props.painting)}
                 />
-                {
-                    this.state.selected && (
-                        <Container fluid
-                            style={{
-                                position: "fixed",
-                                width: "100vw",
-                                height: "100vh",
-                                zIndex: 99,
-                                top: 0, left: 0,
-                                background: "rgba(0, 0, 0, .8)",
-                                color: "rgba(255, 255, 255, .6)",
-                                textAlign: "center",
-                                paddingTop: "5vh"
-                            }}
-
-                            onClick={() => this.setState({ selected: false })}
-                        >
-                            <Row>
-                                <h1 style={{ fontFamily: "Crimson Text", marginTop: 20, padding: "25px 0px" }}>
-                                    <span style={{ fontStyle: "italic", fontWeight: "bold", color: "rgba(255, 255, 255, .85)" }}>{this.props.title}</span> &nbsp;by {this.props.artist}{this.props.created && ", " + this.props.created}
-                                </h1>
-                            </Row>
-                            <Row style={{ padding: 10 }}>
-                                <Col>
-                                    <img
-                                        src={this.props.url_large}
-                                        style={{
-                                            maxHeight: "60vh",
-                                            width: "auto",
-                                            padding: 0
-                                        }}
-                                        alt="something cool"
-                                    />
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                    <Button
-                                        style={{
-                                            background: "rgba(255, 255, 255, 1)",
-                                            border: 0,
-                                            color: "black",
-                                            fontWeight: "bold",
-                                            marginTop: 20,
-                                            marginBottom: 10
-                                        }}
-                                    >
-                                        See More Like This
-                                    </Button>
-                                    &emsp;
-                                    <Button
-                                        style={{
-                                            background: "rgba(220, 53, 69, 1)",
-                                            border: 0,
-                                            color: "white",
-                                            fontWeight: "bold",
-                                            marginTop: 20,
-                                            marginBottom: 10
-                                        }}
-                                    >
-                                        {this.props.likes || 0} Like{!(this.props.likes && this.props.likes === 1) && "s"}
-                                    </Button>
-                                </Col>
-                            </Row>
-                            <br />
-                            <br />
-                            <Row>
-                                <Col>
-                                    Test
-                                </Col>
-                            </Row>
-                        </Container>
-                    )
-                }
             </>
         )
     }
